8ae05a9b667e514ae5a58ecec4a104f27dad331f
Stuart Allen <sallen@wiley.com>
2025-07-16 11:58:45 +0100

    Exclude stories from build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Exclude stories from build
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.stories\.(js|jsx|ts|tsx)$/,
+      use: "ignore-loader",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
